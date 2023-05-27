3bc0ebd5adb64e7780ce169e934c9197e17dd861
VladislavMar <vladm19082006@gmail.com>
2023-05-28 00:20:44 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+//--------------- Вывод в консоль
+// console.log('Helo, from JS!')
+
+//-------------- Переменные
+/*let userName = 'Vlad'
+console.log(userName)
+*/
+
+//------------- Типы переменных
+// Let - Может менять значение во время работы программы
+/*let userName = 'Vlad';
+console.log(userName); 
+*/
+
+/*userName = Ivan;
+console.log(userName);
+*/
+// const - не моедт менять значение
+/*const yearOfBirth = 1990;
+*/
+// var - старый формат объявления перменной
+// не рекомендуется
+// может менять значение на протяжении работы программы
+
+/*var city = 'Moscow'
+var city = 'Minsk'
+var city = 'Kiev'  
+console.log(city)
+*/
+
+//----------------- Область видимости
+// ! По умолчанию используем const
+// когда явно понятно что переменная будет менять свое значение, тогда let
+
+//--------- Типы данных
+let userName = 'Vlad' // Строка (Текст), (Одно словоЮ фраза, предложение, абзац, хоть целая книга)
+ 
+let number = 30 // Число
+
+let = isMarried = false // Логическре занчение true/false
+
+// Динамическая типизация
+let someVariable = 'Some value' // someVariable типа String
+someVariable = 50 // someVariable типа number 
+someVariable = true // someVariable становится типа BooLean
+
+// Значение null 
+// "ничего" "пусто" или "значение неизвестно"
+
+let age = 32
+age = null 
+
+// Значение undefined
+// Значение не присвоено 
+/*let family
+console.log(family)
+ 
+let someName = 'Jeff'
+someName = null
+*/
+
+// оператор typeof
+const someName = true
+typeof someName 
+console.log(typeof someName)
+
+// 
